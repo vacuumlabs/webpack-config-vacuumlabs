7c4999d8d15eedfb48f67c425f9b5000a0149374
Samuel Hapák <samuel.hapak@gmail.com>
2017-04-17 16:39:20 +0200

    FIX: Uglify should be turned on in production.

--- src/optimize.js
+++ src/optimize.js
@@ -2,11 +2,11 @@ import webpack from 'webpack'
 
 export default function(options) {
   return options.env === 'development'
-    ? [new webpack.optimize.UglifyJsPlugin({
+    ? []
+    : [new webpack.optimize.UglifyJsPlugin({
       compress: {
         screw_ie8: true,
         warnings: false, // Because uglify reports irrelevant warnings.
       },
-    })] : []
+    })]
 }
-
